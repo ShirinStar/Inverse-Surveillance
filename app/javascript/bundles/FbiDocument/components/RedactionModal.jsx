4dfe6cc35b05bded0c087dadea96076db9f7c0d6
axylos <robertdraketalley@gmail.com>
2021-03-08 20:23:10 -0800

    add autofocus

--- app/javascript/bundles/FbiDocument/components/RedactionModal.jsx
+++ app/javascript/bundles/FbiDocument/components/RedactionModal.jsx
@@ -79,6 +79,7 @@ export default function RedactionModal(props) {
             onChange={handleChange}
             value={redactionCode}
             name="redactionCode" 
+            autoFocus
             label="Redaction code"/>
              <Tooltip title={
                   <React.Fragment>
