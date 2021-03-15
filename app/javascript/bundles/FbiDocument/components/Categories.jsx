4fa947c606f4a13795ab2a670a185da86f182d5a
shirinstar <shirinanlen@gmail.com>
2021-03-15 11:34:37 -0700

    categories help design

--- app/javascript/bundles/FbiDocument/components/Categories.jsx
+++ app/javascript/bundles/FbiDocument/components/Categories.jsx
@@ -5,90 +5,218 @@ import transcript from 'images/302c.png';
 import CORR from 'images/CORR.png';
 import GEN from 'images/GEN.png';
 import Memo from 'images/Memo.png';
+import markedSample from 'images/markedSample.png';
+import markedFields from 'images/markedFields.jpg';
+import markedRedCode from 'images/markedRedCode.png';
 
 export default function Categories(props) {
-  const {docCat} = props
+  const { docCat } = props
 
   if (docCat == '302s') {
     return (
-      <p>
-       Your type of document usually contains Date, a main section and a buttom section.
-       <Tooltip title={
+      <div className='document-instruction-div'>
+        <p className='document-instruction'>
+          <span className='doc-type'>You received a document that usually contains Date, a main section and a buttom section.</span>
+          <Tooltip title={
             <React.Fragment>
               <img className='hover-image instruction' src={transcript} />
             </React.Fragment>
-            }>
+          }>
             <HelpOutline fontSize="small"></HelpOutline>
           </Tooltip>
-       <br/>
-       Please consider the main section as one long field and give it the label
-       of <strong>'main section'.</strong><br/>
-       For the bottom section, please type/fill the labels of each row.<br/>
-       Sometimes your document may contain addional fields before and after, please
-       include them as well.
-       Please do not forget the redaction codes of each section. If you need help with reading them please visit the 
-       <a className='link-instruction' href='/help'>help page</a>.
+          <br />
+          <br />
+          <span className='instruction-start'>How to start?</span> <br />
+          1• Please type or select a section lable.
+          <Tooltip title={
+            <React.Fragment>
+              <img className='hover-image instruction' src={markedSample} />
+            </React.Fragment>
+          }>
+            <HelpOutline fontSize="small"></HelpOutline>
+          </Tooltip>
+          <br />
+
+          2• Then, fill the text field that will appear below
+          <Tooltip title={
+            <React.Fragment>
+              <img className='hover-image instruction' src={markedFields} />
+            </React.Fragment>
+          }>
+            <HelpOutline fontSize="small"></HelpOutline>
+          </Tooltip> <br />
+
+        3• When there is a redaction, fill the code when asked
+          <Tooltip title={
+            <React.Fragment>
+              <img className='hover-image instruction' src={markedRedCode} />
+            </React.Fragment>
+          }>
+            <HelpOutline fontSize="small"></HelpOutline>
+          </Tooltip>
+          <br />
+        4• Please do that for each new field you're adding.<br />
+        5• Please consider the main section as one long field and give it the label
+          of <strong>'main section'.</strong><br />
+        6• For the bottom section, please fill the labels of each row.<br />
+        7• Sometimes, your document may contain addional fields before and after, please
+          include them as well.<br />
+        8• Before starting, please make sure to visit the instruction page <a className='link-instruction' href='/help'>here </a>.
       </p>
+      </div>
     )
   } else if (docCat == 'CORR') {
     return (
-      <p>
-       Your type of document is letter format.
-       <Tooltip title={
+      <div className='document-instruction-div'>
+        <p className='document-instruction'>
+          <span className='doc-type'> Your type of document is letter format.</span>
+          <Tooltip title={
             <React.Fragment>
               <img className='hover-image instruction' src={CORR} />
             </React.Fragment>
-            }>
+          }>
             <HelpOutline fontSize="small"></HelpOutline>
           </Tooltip>
-       <br/>
-       Please consider the main section of the letter as one long field and give it the label
-       of <strong>'main section'.</strong><br/>
-       Sometimes your document may contain addional fields before and after, please
-       include them as well and type/fill the labels for each row.
-       Please do not forget the redaction codes of each section. If you need help with reading them please visit the 
-       <a className='link-instruction' href='/help'>help page</a>.
-      </p>
+          <br />
+          <br />
+
+        <span className='instruction-start'>How to start?</span> <br />
+        1• Please type or select a section lable.
+        <Tooltip title={
+            <React.Fragment>
+              <img className='hover-image instruction' src={markedSample} />
+            </React.Fragment>
+          }>
+            <HelpOutline fontSize="small"></HelpOutline>
+          </Tooltip>
+          <br />
+        2• Then, fill the text field that will appear below
+        <Tooltip title={
+            <React.Fragment>
+              <img className='hover-image instruction' src={markedFields} />
+            </React.Fragment>
+          }>
+            <HelpOutline fontSize="small"></HelpOutline>
+          </Tooltip> <br />
+        3• When there is a redaction, fill the code when asked
+        <Tooltip title={
+            <React.Fragment>
+              <img className='hover-image instruction' src={markedRedCode} />
+            </React.Fragment>
+          }>
+            <HelpOutline fontSize="small"></HelpOutline>
+          </Tooltip>
+          <br />
+        4• Please do that for each new field you're adding.<br />
+        5• Please consider the main section of the letter as one long field and give it the label
+        of <strong>'main section'.</strong> This section may continue to the following pages.<br />
+        6• Sometimes, your document may contain addional fields before and after, please
+         include them as well.<br />
+        7• Sometime there will be a bottom section, please fill the labels of each row.<br />
+        8• Before starting, please make sure to visit the instruction page <a className='link-instruction' href='/help'>here </a>.
+        </p>
+      </div>
     )
   } else if (docCat == 'GEN') {
     return (
-      <p>
-       Your type of document containd labels and fields.
-       <Tooltip title={
+      <div className='document-instruction-div'>
+        <p className='document-instruction'>
+          <span className='doc-type'>  Your type of document containd labels and fields.</span>
+          <Tooltip title={
             <React.Fragment>
               <img className='hover-image instruction' src={GEN} />
             </React.Fragment>
-            }>
+          }>
             <HelpOutline fontSize="small"></HelpOutline>
           </Tooltip>
-       <br/>
-       Please include all of the field, type/fill the labels for each field.<br/>
-       Usually the label 'Details' contains long text that might continue to a more than one page<br/>
-       Please do not forget the redaction codes of each section. If you need help with reading them please visit the 
-       <a className='link-instruction' href='/help'>help page</a>.
-      </p>
+          <br />
+          <br />
+
+          <span className='instruction-start'>How to start?</span> <br />
+          1• Please type or select a section lable.
+          <Tooltip title={
+            <React.Fragment>
+              <img className='hover-image instruction' src={markedSample} />
+            </React.Fragment>
+          }>
+            <HelpOutline fontSize="small"></HelpOutline>
+          </Tooltip>
+          <br />
+          2• Then, fill the text field that will appear below
+          <Tooltip title={
+            <React.Fragment>
+              <img className='hover-image instruction' src={markedFields} />
+            </React.Fragment>
+          }>
+            <HelpOutline fontSize="small"></HelpOutline>
+          </Tooltip> <br />
+         3• When there is a redaction, fill the code when asked
+         <Tooltip title={
+            <React.Fragment>
+              <img className='hover-image instruction' src={markedRedCode} />
+            </React.Fragment>
+          }>
+            <HelpOutline fontSize="small"></HelpOutline>
+          </Tooltip>
+          <br />
+          4• Please do that for each new field you're adding.<br />
+          5• Usually the label 'Details' contains long text that might continue to a more than one page.<br />
+          6• Before starting, please make sure to visit the instruction page <a className='link-instruction' href='/help'>here </a>.
+        </p>
+      </div>
     )
-  } else if (docCat == 'Memo') {
+  } else if (docCat == 'MEMO') {
     return (
-      <p>
-       Your type of document containd labels and fields.
-       <Tooltip title={
+
+      <div className='document-instruction-div'>
+        <p className='document-instruction'>
+          <span className='doc-type'> Your type of document containd labels and fields. </span>
+          <Tooltip title={
             <React.Fragment>
               <img className='hover-image instruction' src={Memo} />
             </React.Fragment>
-            }>
+          }>
             <HelpOutline fontSize="small"></HelpOutline>
           </Tooltip>
-       <br/>
-       Please include all of the field, type/fill the labels for each field.<br/>
-       Usually the label 'Details' contains long text that might continue to a more than one page<br/>
-       Please do not forget the redaction codes of each section. If you need help with reading them please visit the 
-       <a className='link-instruction' href='/help'>help page</a>.
-      </p>
+          <br />
+          <br />
+
+          <span className='instruction-start'>How to start?</span> <br />
+        1• Please type or select a section lable.
+         <Tooltip title={
+            <React.Fragment>
+              <img className='hover-image instruction' src={markedSample} />
+            </React.Fragment>
+           }>
+            <HelpOutline fontSize="small"></HelpOutline>
+          </Tooltip>
+          <br />
+        2• Then, fill the text field that will appear below
+         <Tooltip title={
+            <React.Fragment>
+              <img className='hover-image instruction' src={markedFields} />
+            </React.Fragment>
+          }>
+            <HelpOutline fontSize="small"></HelpOutline>
+          </Tooltip> <br />
+        3• When there is a redaction, fill the code when asked
+         <Tooltip title={
+            <React.Fragment>
+              <img className='hover-image instruction' src={markedRedCode} />
+            </React.Fragment>
+          }>
+            <HelpOutline fontSize="small"></HelpOutline>
+          </Tooltip>
+          <br />
+        4• Please do that for each new field you're adding.<br />
+        5• Usually the label 'Details' contains long text that might continue to a more than one page.<br />
+        6• Before starting, please make sure to visit the instruction page <a className='link-instruction' href='/help'>here </a>.
+        </p>
+      </div>
     )
   }
-   else {
-     return 'the end'
-   } 
+  else {
+    return 'the end'
+  }
 
 }
--- app/javascript/bundles/FbiDocument/components/InstructionInfo.jsx
+++ app/javascript/bundles/FbiDocument/components/InstructionInfo.jsx
@@ -1,9 +1,4 @@
 import React from 'react';
-import Tooltip from '@material-ui/core/Tooltip';
-import markedSample from 'images/markedSample.png';
-import markedFields from 'images/markedFields.jpg';
-import markedRedCode from 'images/markedRedCode.png';
-import HelpOutline from '@material-ui/icons/HelpOutline';
 import Categories from './Categories';
 
 export default function InstructionInfo(props) {
@@ -27,43 +22,10 @@ export default function InstructionInfo(props) {
       <div className='document-info'>
         <p><strong>Document Date:</strong> {digitalDocument.document_date}</p>
         <p><strong>Page Serial Number:</strong> {startSerialNumber}</p>
-        <p><strong>Current page:</strong> {pageNumber} / {pageCount}</p>
+        <p className='document-info-last'><strong>Current page:</strong> {pageNumber} / {pageCount}</p>
       </div>
       
-      <Categories docCat={docCat}/> <br/><br/>
-
-      <div className='document-instruction-div'>
-        <p className='document-instruction'>General instructions <br />
-          1• Please type or select a section lable.
-          <Tooltip title={
-            <React.Fragment>
-              <img className='hover-image instruction' src={markedSample} />
-            </React.Fragment>
-            }>
-            <HelpOutline fontSize="small"></HelpOutline>
-          </Tooltip>
-          <br />
-          2• Then, fill the text field below
-          <Tooltip title={
-            <React.Fragment>
-              <img className='hover-image instruction' src={markedFields} />
-            </React.Fragment>
-            }>
-            <HelpOutline fontSize="small"></HelpOutline>
-          </Tooltip> <br />
-          3• When there is a redaction, fill the code when asked
-          <Tooltip title={
-            <React.Fragment>
-              <img className='hover-image instruction' src={markedRedCode} />
-            </React.Fragment>
-            }>
-            <HelpOutline fontSize="small"></HelpOutline>
-          </Tooltip>
-         <br /> 
-         <br />
-          Please watch & read the instructions <a className='link-instruction' href='/help'>here </a><br /><br />
-        </p>
-      </div>
+      <Categories docCat={docCat}/>
     </>
   );
 }
