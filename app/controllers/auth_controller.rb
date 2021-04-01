059d1a660e88d4894edba3509396e9d79fe8c727
axylos <robertdraketalley@gmail.com>
2021-03-31 20:37:56 -0700

    tweak resp

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,10 +1,9 @@
 class AuthController < ApplicationController
   def show
-    resp = %{
-    BCCAB17CAA3E5C67E0682CCE8202880E8B140CCF0255010830979C2369922F93
-    comodoca.com
-    eb607abc6428e51
-}
+    resp = %{BCCAB17CAA3E5C67E0682CCE8202880E8B140CCF0255010830979C2369922F93
+comodoca.com
+eb607abc6428e51}
+
     render plain: resp, content_type: 'text/plain'
   end
 end
